13318737434458b3d417dd24346be37b395384ed
MaqaSoltanov <113199901+MaqaSoltanov@users.noreply.github.com>
2022-12-21 14:02:32 +0400

    New homework

--- 5.8 - task - 1/logic.js
+++ 5.8 - task - 1/logic.js
@@ -14,6 +14,8 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
+    updateFinishDates();
+    deleteFunction();
 });
 
 
@@ -21,25 +23,46 @@ fs.readFile('./data.json', 'utf8', (err, data) => {
     const jsonData = JSON.parse(data);
     dataArr.push(...jsonData);
     console.log(data);
-    addToDataBase();
+    // addToDataBase();
 });
 
 function addToDataBase() {
-    for (let i = 0; i < dataArr.length; i++) {
-        let { description, due, employee, finished } = dataArr[i];
+    // for (let i = 0; i < dataArr.length; i++) {
+    //     let { description, due, employee, finished } = dataArr[i];
 
-        if(due == undefined)
-            due = null;
-        if(employee == undefined)
-            employee = null;
-        if(finished == undefined)
-            finished = null;
-        
+    //     if(due == undefined)
+    //         due = "IS NULL";
+    //     if(employee == undefined)
+    //         employee = "IS NULL";
+    //     if(finished == undefined)
+    //         finished = "IS NULL";   
 
-        connection.query(`INSERT INTO tasks (task_description, due_date, employee, finished_date)
-        VALUES ('${description}', '${due}', '${employee}', '${finished}');`,
-            (err, data) => {
-                if (err) console.log(err)
-            })
-    }
+    //     connection.query(`INSERT INTO tasks (task_description, due_date, employee, finished_date)
+    //     VALUES ('${description}', '${due}', '${employee}', '${finished}');`,
+    //         (err, data) => {
+    //             if (err) console.log(err)
+    //         })
 }
+
+function updateFinishDates() {
+    let currentDate = '2022-12-21';
+    console.log(currentDate);
+    connection.query(`UPDATE tasks SET due_date = '${currentDate}' WHERE due_date = 'IS NULL';`, (err, data) => {
+        if (!err) {
+            console.log(data);
+        }
+
+        console.log(err);
+    });
+}
+
+function deleteFunction() {
+    connection.query(`DELETE FROM tasks
+    WHERE due_date < finished_date;`
+    ,(err,data) => {
+        console.log("DATA");
+        console.log(data);
+    }
+)
+}
+
